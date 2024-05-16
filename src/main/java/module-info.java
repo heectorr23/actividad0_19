755016f77e952fd4d181a11d298726a685fd64e2
Hector Olivera <hector.olivera@campusfp.es>
2024-05-16 09:39:08 +0200

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.empresa.actividad0_19 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.empresa.actividad0_19 to javafx.fxml;
+    exports com.empresa.actividad0_19;
+}
